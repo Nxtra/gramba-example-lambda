5d45647be610652f326af62a62224f1e1b8df6fd
Mitch Mommers <mitchoost@gmail.com>
2020-02-26 11:49:38 +0100

    Initial commit

---
+++ src/main/java/be/cloudway/gramba/test/lambda/GrambaHandler.java
@@ -0,0 +1,50 @@
+package be.cloudway.gramba.test.lambda;
+
+import be.cloudway.gramba.runtime.handler.GrambaLambdaHandler;
+import be.cloudway.gramba.runtime.GrambaRuntime;
+import be.cloudway.gramba.runtime.model.ApiResponse;
+import be.cloudway.gramba.runtime.aws.runtime.implementation.ContextImpl;
+import be.cloudway.gramba.runtime.eventrunner.RunOnceEventRunner;
+import be.cloudway.gramba.runtime.helpers.JacksonHelper;
+import be.cloudway.grambda.runtime.dev.addons.commands.CommandHandler;
+import be.cloudway.grambda.runtime.dev.addons.commands.FileLoader;
+import be.cloudway.grambda.runtime.dev.addons.model.GrambaSettings;
+import be.cloudway.grambda.runtime.dev.addons.strategy.MockAwsApiStrategy;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+import java.util.List;
+import java.util.Map;
+
+public class GrambaHandler implements GrambaLambdaHandler<APIGatewayProxyResponseEvent, ContextImpl> {
+    private static final Handler handler = new Handler();
+    private static final GrambaRuntime gramba = new GrambaRuntime(new GrambaHandler());
+
+    public APIGatewayProxyResponseEvent customHandler(ApiResponse body, Map<String, List<String>> headers, JacksonHelper jacksonHelper) {
+        APIGatewayProxyRequestEvent awsApiRequest =
+                jacksonHelper.toObject(body.getResponse(), APIGatewayProxyRequestEvent.class);
+
+        return handler.handleRequest(awsApiRequest, getContext());
+    }
+
+    public ContextImpl getContext() {
+        return new ContextImpl();
+    }
+
+    public static void main (String... args) {
+        if (args.length > 0) {
+            GrambaSettings settings = new CommandHandler().validateArgs(args);
+            if (settings.isTestMode()) {
+                FileLoader fileLoader = new FileLoader();
+                String testFile = fileLoader.loadFileAsString(settings.getPathToMockJson());
+
+                MockAwsApiStrategy mock = new MockAwsApiStrategy(testFile);
+                new GrambaRuntime(new RunOnceEventRunner(new GrambaHandler(), mock)).runEventRunner();
+
+                return;
+            }
+        }
+
+        gramba.runEventRunner();
+    }
+}
---
+++ src/main/java/be/cloudway/gramba/test/lambda/Handler.java
@@ -0,0 +1,21 @@
+package be.cloudway.gramba.test.lambda;
+
+
+import be.cloudway.gramba.test.lambda.model.ResponseObject;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+import static be.cloudway.gramba.runtime.GrambaRuntime.STATIC_REFERENCES;
+
+public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+        APIGatewayProxyResponseEvent apiGatewayProxyResponseEvent = new APIGatewayProxyResponseEvent();
+        apiGatewayProxyResponseEvent.setStatusCode(200);
+        apiGatewayProxyResponseEvent.setBody(STATIC_REFERENCES.jacksonHelper.fromObj(new ResponseObject()));
+
+        return apiGatewayProxyResponseEvent;
+    }
+}
